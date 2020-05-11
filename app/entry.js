cd7322436f3269a39ece79d4a187dcad5f22fd31
tk-code-dev <reiwa.dupuis.2019@gmail.com>
2020-05-11 19:09:40 +0000

    display connect event on console

--- app/entry.js
+++ app/entry.js
@@ -17,8 +17,16 @@ movingButton.click(() => {
 
 const loadavg = $('#loadavg');
 
-setInterval(() => {
-  $.get('/server-status', {}, (data) => {
-    loadavg.text(data.loadavg.toString());
-  });
-}, 10);
+// setInterval(() => {
+//   $.get('/server-status', {}, (data) => {
+//     loadavg.text(data.loadavg.toString());
+//   });
+// }, 10);
+
+import io from 'socket.io-client';
+ const socket = io('http://localhost:8000');
+ socket.on('server-status', (data) => {
+   loadavg.text(data.loadavg.toString());
+ });
+socket.on('connect', () => { console.log('接続しました'); });
+socket.on('disconnect', () => { console.log('切断しました'); });
